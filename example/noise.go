e3ad804b719d73ca5e4668202e9997a05a9ade7b
jjwygjj <wangyaoguang@cel.la>
2020-12-18 18:30:35 +0800

    feat(example):add noise app

--- example/noise.go
+++ example/noise.go
@@ -9,11 +9,14 @@ import (
 )
 
 func Handler(rxstream rx.RxStream) rx.RxStream {
-	stream := rxstream.Y3Decoder("0x10", float32(0)).AuditTime(100 * time.Millisecond).Map(func(_ context.Context, i interface{}) (interface{}, error) {
-		value := i.(float32)
-		fmt.Println("serverless get value:", value)
-		return value, nil
-	}).StdOut()
+	stream := rxstream.
+		Y3Decoder("0x10", float32(0)).
+		AuditTime(100 * time.Millisecond).
+		Map(func(_ context.Context, i interface{}) (interface{}, error) {
+			value := i.(float32)
+			fmt.Println("serverless get value:", value)
+			return value, nil
+		}).StdOut()
 
 	return stream
 }
