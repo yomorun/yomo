78a03160bf1c141c527918b5cd7d525f47b114cc
Hong Xiaojian <xiaojian.hong@gmail.com>
2021-08-24 21:31:18 +0800

    fix(sfn): run Handler without goroutine

--- streamfunction/client.go
+++ streamfunction/client.go
@@ -111,7 +111,7 @@ func (c *clientImpl) readStream(stream quic.ReceiveStream, handler func(rxstream
 			break
 		}
 
-		go c.executeHandler(ctx, cancel, item.V, handler, fac)
+		c.executeHandler(ctx, cancel, item.V, handler, fac)
 		// one data per time.
 		break
 	}
