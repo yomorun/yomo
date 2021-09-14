5895f9187c736c60dd692d74e0238b95fffea390
fanweixiao <fanweixiao@gmail.com>
2021-09-14 10:42:41 +0800

    add comment

--- severless.go
+++ severless.go
@@ -14,14 +14,19 @@ const (
 	StreamFunctionLogPrefix = "\033[31m[yomo:sfn]\033[0m "
 )
 
+// StreamFunction defines serverless streaming functions.
 type StreamFunction interface {
+	// SetObserveDataID set the data id list that will be observed
 	SetObserveDataID(id ...uint8)
+	// SetHandler set the handler function, which accept the raw bytes data and return the tag & response
 	SetHandler(fn func([]byte) (byte, []byte)) error
+	// Connect create a connection to the zipper
 	Connect() error
+	// Close will close the connection
 	Close() error
 }
 
-// NewStreamFunction create a stream function
+// NewStreamFunction create a stream function.
 func NewStreamFunction(name string, opts ...Option) *streamFunction {
 	options := newOptions(opts...)
 	client := core.NewClient(name, core.ConnTypeStreamFunction)
@@ -30,7 +35,6 @@ func NewStreamFunction(name string, opts ...Option) *streamFunction {
 		zipperEndpoint: options.ZipperAddr,
 		client:         client,
 		observed:       make([]uint8, 0),
-		// logger:         utils.DefaultLogger.WithPrefix("\033[31m[yomo:sfn]\033[0m"),
 	}
 
 	return &sfn
@@ -38,40 +42,40 @@ func NewStreamFunction(name string, opts ...Option) *streamFunction {
 
 var _ StreamFunction = &streamFunction{}
 
-// Steaming StreamFunction 定义
+// streamFunction implements StreamFunction interface.
 type streamFunction struct {
 	name           string
 	zipperEndpoint string
 	client         *core.Client
-	// logger         utils.Logger
-	observed []uint8                     // StreamFunction 监听的数据 ID
-	fn       func([]byte) (byte, []byte) // StreamFunction 的方法
+	observed       []uint8                     // ID list that will be observed
+	fn             func([]byte) (byte, []byte) // user's function which will be invoked when data arrived
 }
 
-// 设置监听的 DataID 列表，当列表中对应 ID 的数据到达时，将其放入 RxStream 中，
-// 只有一个 RxStream 对象，用户可以通过 RxStream.Map() 方法再繁殖出新的逻辑流，
-// 进而再进行操作
+// SetObserveDataID set the data id list that will be observed.
 func (s *streamFunction) SetObserveDataID(id ...uint8) {
 	s.observed = append(s.observed, id...)
 	logger.Debugf("%sSetObserveDataID(%v)", StreamFunctionLogPrefix, s.observed)
 }
 
-// 注入 Handler() 回调
+// SetHandler set the handler function, which accept the raw bytes data and return the tag & response.
 func (s *streamFunction) SetHandler(fn func([]byte) (byte, []byte)) error {
 	s.fn = fn
 	logger.Debugf("%sSetHandler(%v)", StreamFunctionLogPrefix, s.fn)
 	return nil
 }
 
-// 开始连接到 Zipper，接收到的数据将被 SetHandler() 方法注入的 func_ 处理
+// Connect create a connection to the zipper, when data arrvied, the data will be passed to the
+// handler which setted by SetHandler method.
 func (s *streamFunction) Connect() error {
+	// TODO: should guarante SetHandler() has been called
 	logger.Debugf("%s Connect()", StreamFunctionLogPrefix)
-	// 注册给底层的 quic-client，当收到 DataFrame 时，转发过来
+	// notify underlying network operations, when data with tag we observed arrived, invoke the func
 	s.client.SetDataFrameObserver(func(tag byte, carraige []byte, metaFrame MetaFrame) {
 		for _, t := range s.observed {
 			if t == tag {
 				logger.Debugf("%sreceive DataFrame, tag=%# x, carraige=%# x", StreamFunctionLogPrefix, tag, carraige)
 				s.onDataFrame(carraige, metaFrame)
+				// TODO: consider remove return
 				return
 			}
 		}
@@ -79,14 +83,12 @@ func (s *streamFunction) Connect() error {
 
 	err := s.client.Connect(context.Background(), s.zipperEndpoint)
 	if err != nil {
-		// 创建连接失败
 		logger.Errorf("%sConnect() error: %s", StreamFunctionLogPrefix, err)
-		return err
 	}
-	return nil
+	return err
 }
 
-// 关闭连接
+// Close will close the connection.
 func (s *streamFunction) Close() error {
 	if s.client != nil {
 		if err := s.client.Close(); err != nil {
@@ -97,23 +99,29 @@ func (s *streamFunction) Close() error {
 	return nil
 }
 
+// when DataFrame we observed arrived, invoke the user's function
 func (s *streamFunction) onDataFrame(data []byte, metaFrame MetaFrame) {
+	// check
+	if s.fn == nil {
+		logger.Warnf("%sStreamFunction is nil", StreamFunctionLogPrefix)
+		return
+	}
 	// tracing
 	span, err := tracing.NewRemoteTraceSpan(metaFrame.Get("TraceID"), metaFrame.Get("SpanID"), "serverless", fmt.Sprintf("onDataFrame [%s]", s.name))
 	if err == nil {
 		defer span.End()
 	}
-	if s.fn == nil {
-		logger.Warnf("%sStreamFunction is nil", StreamFunctionLogPrefix)
-		return
-	}
 	logger.Infof("%sonDataFrame metadata=%s, [%s]->[%s]", StreamFunctionLogPrefix, metaFrame.GetMetadatas(), metaFrame.GetIssuer(), s.name)
 	logger.Debugf("%sexecute-start fn: data=%#x", StreamFunctionLogPrefix, data)
+	// invoke serverless
 	tag, resp := s.fn(data)
 	logger.Debugf("%sexecute-done fn: tag=%#x, resp=%#x", StreamFunctionLogPrefix, tag, resp)
-	// resp 是用户返回的数据，如果不为空，要发送回给 zipper
+	// if resp is not nil, means the user's function has returned something, we should send it to the zipper
+	// TODO: a better way to explict the return value
 	if len(resp) != 0 {
 		logger.Debugf("%sstart WriteFrame(): tag=%#x, data=%v", StreamFunctionLogPrefix, tag, resp)
+		// build a DataFrame
+		// TODO: seems we should implement a DeepCopy() of MetaFrame
 		frame := frame.NewDataFrame(metaFrame.GetMetadatas()...)
 		frame.SetIssuer(s.name)
 		frame.SetCarriage(tag, resp)
