93a7d730722926492bfa1a7ae91b2d745194ee3c
venjiang <venjiang@gmail.com>
2021-09-17 21:09:27 +0800

    style(cli): clean debug codes

--- cli/serverless/golang/serverless.go
+++ cli/serverless/golang/serverless.go
@@ -71,7 +71,6 @@ func (s *GolangServerless) Init(opts *serverless.Options) error {
 	// Create the AST by parsing src
 	fset := token.NewFileSet()
 	astf, err := parser.ParseFile(fset, "", source, 0)
-	fmt.Println(string(source))
 	if err != nil {
 		return fmt.Errorf("Init: parse source file err %s", err)
 	}
