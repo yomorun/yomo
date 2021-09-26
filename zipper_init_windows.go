c43e4d8d0b207b80e14f7d51c84a8322c9438088
venjiang <venjiang@gmail.com>
2021-09-26 15:30:00 +0800

    style: update logger print

--- zipper_init_windows.go
+++ zipper_init_windows.go
@@ -17,7 +17,7 @@ func (z *zipper) init() {
 	go func() {
 		c := make(chan os.Signal, 1)
 		signal.Notify(c, os.Interrupt, syscall.SIGTERM, syscall.SIGINT)
-		logger.Printf("%sListening SIGUSR1, SIGUSR2, SIGTERM/SIGINT...", zipperLogPrefix)
+		logger.Printf("%sListening SIGTERM/SIGINT...", zipperLogPrefix)
 		for p1 := range c {
 			logger.Printf("Received signal: %s", p1)
 			if p1 == syscall.SIGTERM || p1 == syscall.SIGINT {
