ccc5883684c837767db92ff351c1958906cfe846
Junzhuo Wu <wujz@cloudbrain.ai>
2022-01-13 16:33:46 +0800

    style: comments on handler types

--- core/handler_type.go
+++ core/handler_type.go
@@ -4,5 +4,8 @@ import (
 	"github.com/yomorun/yomo/core/frame"
 )
 
+// SimpleHandler is the request-response mode (asnyc)
 type SimpleHandler func([]byte) (byte, []byte)
+
+// PipeHandler is the bidirectional stream mode (blocking).
 type PipeHandler func(in <-chan []byte, out chan<- *frame.PayloadFrame)
