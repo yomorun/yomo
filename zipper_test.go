8c59b06f97345e6c05580b8748f28a252f085fdc
venjiang <venjiang@gmail.com>
2022-03-01 14:57:25 +0800

    zipper test

---
+++ zipper_test.go
@@ -0,0 +1,17 @@
+package yomo
+
+import (
+	"testing"
+	"time"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestZipperRun(t *testing.T) {
+	zipper := NewZipperWithOptions("zipper", WithZipperAddr("localhost:9001"))
+	time.Sleep(time.Second)
+	assert.NotNil(t, zipper)
+	err := zipper.Close()
+	time.Sleep(time.Second)
+	assert.Nil(t, err)
+}
