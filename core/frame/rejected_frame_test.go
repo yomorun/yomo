0c94239c9a4cacddead2c7aba6d25e0234b07471
venjiang <venjiang@gmail.com>
2022-04-25 20:10:46 +0800

    fix test

--- core/frame/rejected_frame_test.go
+++ core/frame/rejected_frame_test.go
@@ -7,13 +7,13 @@ import (
 )
 
 func TestRejectedFrameEncode(t *testing.T) {
-	f := NewRejectedFrame()
-	assert.Equal(t, []byte{0x80 | byte(TagOfRejectedFrame), 0x00}, f.Encode())
+	f := NewRejectedFrame("")
+	assert.Equal(t, []byte{0x80 | byte(TagOfRejectedFrame), 0x02, 0x02, 0x00}, f.Encode())
 }
 
 func TestRejectedFrameDecode(t *testing.T) {
 	buf := []byte{0x80 | byte(TagOfRejectedFrame), 0x00}
 	ping, err := DecodeToRejectedFrame(buf)
 	assert.NoError(t, err)
-	assert.Equal(t, []byte{0x80 | byte(TagOfRejectedFrame), 0x00}, ping.Encode())
+	assert.Equal(t, []byte{0x80 | byte(TagOfRejectedFrame), 0x2, 0x2, 0x0}, ping.Encode())
 }
