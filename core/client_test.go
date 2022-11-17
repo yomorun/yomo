5ec038c72ec4d2678f3aadae1f947c8ef247995e
woorui <rui1009479218@gmail.com>
2022-11-17 18:23:03 +0800

    test: add client unittest

--- core/client_test.go
+++ core/client_test.go
@@ -2,7 +2,6 @@ package core
 
 import (
 	"context"
-	"fmt"
 	"testing"
 	"time"
 
@@ -12,6 +11,7 @@ import (
 	"github.com/yomorun/yomo/core/metadata"
 	"github.com/yomorun/yomo/core/router"
 	"github.com/yomorun/yomo/pkg/config"
+	"github.com/yomorun/yomo/pkg/logger"
 )
 
 const testaddr = "127.0.0.1:19999"
@@ -34,6 +34,11 @@ func Test_Client_Dial_Nothing(t *testing.T) {
 func Test_Frame_RoundTrip(t *testing.T) {
 	ctx := context.Background()
 
+	var (
+		obversedTag = frame.Tag(1)
+		payload     = []byte("hello data frame")
+	)
+
 	server := NewServer("zipper",
 		WithAddr(testaddr),
 		WithAuth("token", "auth-token"),
@@ -46,38 +51,45 @@ func Test_Frame_RoundTrip(t *testing.T) {
 		server.ListenAndServe(ctx, testaddr)
 	}()
 
-	t.Run("source write data to server", func(t *testing.T) {
-		source := NewClient("source", ClientTypeSource, WithCredential("token:auth-token"))
+	source := NewClient(
+		"source",
+		ClientTypeSource,
+		WithCredential("token:auth-token"),
+		WithObserveDataTags(obversedTag),
+		WithClientQuicConfig(DefalutQuicConfig),
+		WithClientTLSConfig(nil),
+		WithLogger(logger.Default()),
+	)
 
-		source.SetBackflowFrameObserver(func(bf *frame.BackflowFrame) {
-			fmt.Println(bf)
-		})
-
-		err := source.Connect(ctx, testaddr)
-		assert.Nil(t, err, "source connect must be success")
-		assert.Equal(t, ConnStateConnected, source.State(), "source state should be ConnStateReady")
-
-		dataFrame := frame.NewDataFrame()
-		dataFrame.SetSourceID(source.clientID)
-		dataFrame.SetCarriage(frame.Tag(1), []byte("hello data frame"))
-
-		err = source.WriteFrame(dataFrame)
-		assert.Nil(t, err, "source write dataFrame must be success")
+	source.SetBackflowFrameObserver(func(bf *frame.BackflowFrame) {
+		assert.Equal(t, string(payload), string(bf.GetCarriage()))
 	})
 
-	t.Run("sfn accept data from server", func(t *testing.T) {
-		source := NewClient("sfn-1", ClientTypeStreamFunction, WithCredential("token:auth-token"))
+	err := source.Connect(ctx, testaddr)
+	assert.Nil(t, err, "source connect must be success")
+	assert.Equal(t, ConnStateConnected, source.State(), "source state should be ConnStateReady")
 
-		source.SetDataFrameObserver(func(bf *frame.DataFrame) {
-			fmt.Println(bf)
-		})
+	sfn := NewClient("sfn-1", ClientTypeStreamFunction, WithCredential("token:auth-token"), WithObserveDataTags(obversedTag))
 
-		err := source.Connect(ctx, testaddr)
-		assert.Nil(t, err, "sfn connect must be success")
-		assert.Equal(t, ConnStateConnected, source.State(), "sfn state should be ConnStateReady")
+	sfn.SetDataFrameObserver(func(bf *frame.DataFrame) {
+		assert.Equal(t, string(payload), string(bf.GetCarriage()))
 	})
 
-	time.Sleep(20 * time.Second)
+	err = sfn.Connect(ctx, testaddr)
+	assert.Nil(t, err, "sfn connect must be success")
+	assert.Equal(t, ConnStateConnected, sfn.State(), "sfn state should be ConnStateReady")
+
+	// wait source and sfn handshake successful (not elegant).
+	time.Sleep(time.Second)
+
+	dataFrame := frame.NewDataFrame()
+	dataFrame.SetSourceID(source.clientID)
+	dataFrame.SetCarriage(obversedTag, payload)
+
+	err = source.WriteFrame(dataFrame)
+	assert.Nil(t, err, "source write dataFrame must be success")
+
+	time.Sleep(time.Second)
 	// TODO: closing server many times is blocking.
 	assert.Nil(t, server.Close(), "server.Close() should not return error")
 }
