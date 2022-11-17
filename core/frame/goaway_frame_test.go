0cb555b593c514cb2a25b248922d0b8146103b47
woorui <rui1009479218@gmail.com>
2022-11-17 22:28:39 +0800

    test: frame

---
+++ core/frame/goaway_frame_test.go
@@ -0,0 +1,19 @@
+package frame
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestGoawayFrameEncode(t *testing.T) {
+	f := NewGoawayFrame("goaway")
+	assert.Equal(t, []byte{0x80 | byte(TagOfGoawayFrame), 0x8, 0x2, 0x6, 0x67, 0x6f, 0x61, 0x77, 0x61, 0x79}, f.Encode())
+}
+
+func TestGoawayFrameDecode(t *testing.T) {
+	buf := []byte{0x80 | byte(TagOfGoawayFrame), 0x8, 0x2, 0x6, 0x67, 0x6f, 0x61, 0x77, 0x61, 0x79}
+	f, err := DecodeToGoawayFrame(buf)
+	assert.NoError(t, err)
+	assert.Equal(t, []byte{0x80 | byte(TagOfGoawayFrame), 0x8, 0x2, 0x6, 0x67, 0x6f, 0x61, 0x77, 0x61, 0x79}, f.Encode())
+}
