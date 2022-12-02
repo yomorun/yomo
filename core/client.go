0b961ebca5bfe4688a6448d6d49575b510bc7ed9
woorui <rui1009479218@gmail.com>
2022-12-02 17:24:24 +0800

    fix: Addr to RemoteAddr

--- core/client.go
+++ core/client.go
@@ -297,8 +297,8 @@ func (c *Client) reconnect(ctx context.Context, addr string) {
 	}
 }
 
-// Addr returns the address of the client.
-func (c *Client) Addr() string { return c.addr }
+// RemoteAddr returns the remote address of the client.
+func (c *Client) RemoteAddr() string { return c.addr }
 
 // SetObserveDataTags set the data tag list that will be observed.
 // Deprecated: use yomo.WithObserveDataTags instead
@@ -338,4 +338,4 @@ func (c *Client) State() ConnState {
 }
 
 // String returns client's name and addr format as a string.
-func (c *Client) String() string { return fmt.Sprintf("name:%s, addr: %s", c.name, c.Addr()) }
+func (c *Client) String() string { return fmt.Sprintf("name:%s, addr: %s", c.name, c.RemoteAddr()) }
