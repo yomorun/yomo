f77d8ee51580af6e63a8ba59668b79dab32a1321
woorui <rui1009479218@gmail.com>
2023-04-07 02:43:25 +0800

    docs: polish metadata code comment

--- core/metadata/metadata.go
+++ core/metadata/metadata.go
@@ -4,9 +4,11 @@ package metadata
 // Metadata is an interface used to store additional information about the application.
 //
 //	There are three types of metadata in yomo:
-//	 1. Metadata from `Authentication.Authenticate()`, This means that the metadata is built in the control stream.
-//	 2. Metadata from the data stream.
-//	 3. Metadata in DataFrame.
+//	 1. Metadata from `Authentication.Authenticate()`, This is connection-level metadata.
+//	 2. Metadata from the handshake, This is stream-level metadata.
+//	 3. Metadata from the DataFrame, This is frame-level metadata.
+//
+// These types of metadata can be merged together to route the SFN.
 type Metadata interface {
 	// Encode encodes the metadata into a byte slice.
 	Encode() ([]byte, error)
