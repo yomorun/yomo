a436ab5292e2a64506e069cf43854a5594745a0c
C.C <fanweixiao@gmail.com>
2023-04-16 23:41:49 +0800

    docs: cli, concept and glossary (#502)

--- docs/pages/_document.js
+++ docs/pages/_document.js
@@ -7,6 +7,23 @@ class MyDocument extends Document {
     return (
       <Html lang="en">
         <Head>
+          {/* Global Site Tag (gtag.js) - Google Analytics */}
+          <script
+            async
+            src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GA_ID}`}
+          />
+          <script
+            dangerouslySetInnerHTML={{
+              __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', '${process.env.NEXT_PUBLIC_GA_ID}', {
+              page_path: window.location.pathname,
+            });
+          `,
+            }}
+          />
           <link href="https://fonts.googleapis.com/css2?family=Exo+2&display=swap" rel="stylesheet"></link>
         </Head>
         <body>
