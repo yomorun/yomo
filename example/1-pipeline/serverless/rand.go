7524bda08c8b2e3f577d1b69bee428f21c015289
C.C <fanweixiao@gmail.com>
2023-04-28 13:54:50 +0800

    fix examples

--- example/1-pipeline/serverless/rand.go
+++ example/1-pipeline/serverless/rand.go
@@ -3,18 +3,16 @@ package main
 import (
 	"encoding/binary"
 	"log"
-
-	"github.com/yomorun/yomo/core/frame"
 )
 
 // Handler will handle the raw data.
-func Handler(data []byte) (frame.Tag, []byte) {
+func Handler(data []byte) (uint32, []byte) {
 	randint := binary.LittleEndian.Uint32(data)
 	log.Printf("Generate random uint32: %d (%# x)", randint, data)
 	return 0, nil
 }
 
 // DataTags describes the type of data this serverless function observed.
-func DataTags() []frame.Tag {
-	return []frame.Tag{0x01}
+func DataTags() []uint32 {
+	return []uint32{0x01}
 }
