82e17ff4e230cd57212dd5e2719e8a6374c5c7fe
wurui <rui1009479218@gmail.com>
2023-10-20 23:33:43 +0800

    feat(log): better log outputs when dispatching to downstreams (#644)

--- core/client_test.go
+++ core/client_test.go
@@ -16,7 +16,6 @@ import (
 	"github.com/yomorun/yomo/core/router"
 	"github.com/yomorun/yomo/core/ylog"
 	"github.com/yomorun/yomo/pkg/frame-codec/y3codec"
-	"github.com/yomorun/yomo/pkg/id"
 )
 
 const testaddr = "127.0.0.1:19999"
@@ -29,8 +28,8 @@ var discardingLogger = ylog.NewFromConfig(ylog.Config{Output: "/dev/null", Error
 func TestClientDialNothing(t *testing.T) {
 	ctx := context.Background()
 
-	client := NewClient("source", ClientTypeSource, WithLogger(discardingLogger))
-	err := client.Connect(ctx, testaddr)
+	client := NewClient("source", testaddr, ClientTypeSource, WithLogger(discardingLogger))
+	err := client.Connect(ctx)
 
 	qerr := net.ErrClosed
 	assert.ErrorAs(t, err, &qerr, "dial must timeout")
@@ -60,22 +59,23 @@ func TestFrameRoundTrip(t *testing.T) {
 	server.SetBeforeHandlers(ht.beforeHandler)
 	server.SetAfterHandlers(ht.afterHandler)
 
-	recorder := newFrameWriterRecorder("mockClient")
-	server.AddDownstreamServer("mockAddr", recorder)
+	recorder := newFrameWriterRecorder("mockID", "mockClientLocal", "mockClientRemote")
+	server.AddDownstreamServer(recorder)
 
-	assert.Equal(t, server.Downstreams()["mockAddr"], recorder.ClientID())
+	assert.Equal(t, server.Downstreams()["mockID"], recorder.ID())
 
 	go func() {
 		err := server.ListenAndServe(ctx, testaddr)
 		fmt.Println(err)
 	}()
 
-	illegalTokenSource := NewClient("source", ClientTypeSource, WithCredential("token:error-token"), WithLogger(discardingLogger))
-	err := illegalTokenSource.Connect(ctx, testaddr)
+	illegalTokenSource := NewClient("source", testaddr, ClientTypeSource, WithCredential("token:error-token"), WithLogger(discardingLogger))
+	err := illegalTokenSource.Connect(ctx)
 	assert.Equal(t, "authentication failed: client credential name is token", err.Error())
 
 	source := NewClient(
 		"source",
+		testaddr,
 		ClientTypeSource,
 		WithCredential("token:auth-token"),
 		WithClientQuicConfig(DefalutQuicConfig),
@@ -90,10 +90,10 @@ func TestFrameRoundTrip(t *testing.T) {
 		assert.Equal(t, string(backflow), string(bf.Carriage))
 	})
 
-	err = source.Connect(ctx, testaddr)
+	err = source.Connect(ctx)
 	assert.NoError(t, err, "source connect must be success")
-	closeEarlySfn := createTestStreamFunction("close-early-sfn", observedTag)
-	closeEarlySfn.Connect(ctx, testaddr)
+	closeEarlySfn := createTestStreamFunction("close-early-sfn", testaddr, observedTag)
+	closeEarlySfn.Connect(ctx)
 	assert.Equal(t, nil, err)
 
 	// test close early.
@@ -104,7 +104,7 @@ func TestFrameRoundTrip(t *testing.T) {
 	assert.True(t, exited, "close-early-sfn should exited")
 
 	// sfn to zipper.
-	sfn := createTestStreamFunction("sfn-1", observedTag)
+	sfn := createTestStreamFunction("sfn-1", testaddr, observedTag)
 	sfn.SetDataFrameObserver(func(bf *frame.DataFrame) {
 		assert.Equal(t, string(payload), string(bf.Payload))
 
@@ -123,7 +123,7 @@ func TestFrameRoundTrip(t *testing.T) {
 		}
 	})
 
-	err = sfn.Connect(ctx, testaddr)
+	err = sfn.Connect(ctx)
 	assert.NoError(t, err, "sfn connect should replace the old sfn stream")
 
 	exited = checkClientExited(sfn, time.Second)
@@ -211,9 +211,10 @@ func (a *hookTester) afterHandler(ctx *Context) error {
 	return nil
 }
 
-func createTestStreamFunction(name string, observedTag frame.Tag) *Client {
+func createTestStreamFunction(name string, zipperAddr string, observedTag frame.Tag) *Client {
 	sfn := NewClient(
 		name,
+		zipperAddr,
 		ClientTypeStreamFunction,
 		WithCredential("token:auth-token"),
 		WithLogger(discardingLogger),
@@ -226,27 +227,30 @@ func createTestStreamFunction(name string, observedTag frame.Tag) *Client {
 // frameWriterRecorder frames be writen.
 type frameWriterRecorder struct {
 	id           string
-	name         string
+	localName    string
+	remoteName   string
 	codec        frame.Codec
 	packetReader frame.PacketReadWriter
 	mu           sync.Mutex
 	buf          *bytes.Buffer
 }
 
-func newFrameWriterRecorder(name string) *frameWriterRecorder {
+func newFrameWriterRecorder(id, localName, remoteName string) *frameWriterRecorder {
 	return &frameWriterRecorder{
-		id:           id.New(),
-		name:         name,
+		id:           id,
+		localName:    localName,
+		remoteName:   remoteName,
 		codec:        y3codec.Codec(),
 		packetReader: y3codec.PacketReadWriter(),
 		buf:          new(bytes.Buffer),
 	}
 }
 
-func (w *frameWriterRecorder) ClientID() string                          { return w.id }
-func (w *frameWriterRecorder) Name() string                              { return w.name }
-func (w *frameWriterRecorder) Close() error                              { return nil }
-func (w *frameWriterRecorder) Connect(_ context.Context, _ string) error { return nil }
+func (w *frameWriterRecorder) ID() string                      { return w.id }
+func (w *frameWriterRecorder) LocalName() string               { return w.localName }
+func (w *frameWriterRecorder) RemoteName() string              { return w.remoteName }
+func (w *frameWriterRecorder) Close() error                    { return nil }
+func (w *frameWriterRecorder) Connect(_ context.Context) error { return nil }
 
 func (w *frameWriterRecorder) WriteFrame(f frame.Frame) error {
 	w.mu.Lock()
--- core/server.go
+++ core/server.go
@@ -45,7 +45,7 @@ type Server struct {
 	codec                   frame.Codec
 	packetReadWriter        frame.PacketReadWriter
 	counterOfDataFrame      int64
-	downstreams             map[string]FrameWriterConnection
+	downstreams             map[string]Downstream
 	mu                      sync.Mutex
 	opts                    *serverOptions
 	startHandlers           []FrameHandler
@@ -73,7 +73,7 @@ func NewServer(name string, opts ...ServerOption) *Server {
 		ctx:              ctx,
 		ctxCancel:        ctxCancel,
 		name:             name,
-		downstreams:      make(map[string]FrameWriterConnection),
+		downstreams:      make(map[string]Downstream),
 		logger:           logger,
 		tracerProvider:   options.tracerProvider,
 		codec:            y3codec.Codec(),
@@ -96,8 +96,8 @@ func (s *Server) ListenAndServe(ctx context.Context, addr string) error {
 	}
 
 	// connect to all downstreams.
-	for addr, client := range s.downstreams {
-		go client.Connect(ctx, addr)
+	for _, client := range s.downstreams {
+		go client.Connect(ctx)
 	}
 
 	return s.Serve(ctx, conn)
@@ -149,7 +149,7 @@ func (s *Server) handleConnection(qconn quic.Connection, fs *FrameStream, logger
 	}
 
 	logger = logger.With("conn_id", conn.ID(), "conn_name", conn.Name())
-	logger.Info("client connected", "remote_addr", qconn.RemoteAddr().String(), "client_type", conn.ClientType().String())
+	logger.Info("new client connected", "remote_addr", qconn.RemoteAddr().String(), "client_type", conn.ClientType().String())
 
 	c := newContext(conn, route, logger)
 
@@ -300,7 +300,7 @@ func (s *Server) Close() error {
 	return nil
 }
 
-func closeServer(downstreams map[string]FrameWriterConnection, connector *Connector, listener *quic.Listener, router router.Router) error {
+func closeServer(downstreams map[string]Downstream, connector *Connector, listener *quic.Listener, router router.Router) error {
 	for _, ds := range downstreams {
 		ds.Close()
 	}
@@ -482,7 +482,7 @@ func (s *Server) Downstreams() map[string]string {
 
 	snapshotOfDownstream := make(map[string]string, len(s.downstreams))
 	for addr, client := range s.downstreams {
-		snapshotOfDownstream[addr] = client.ClientID()
+		snapshotOfDownstream[addr] = client.ID()
 	}
 	return snapshotOfDownstream
 }
@@ -497,9 +497,9 @@ func (s *Server) ConfigRouter(router router.Router) {
 
 // AddDownstreamServer add a downstream server to this server. all the DataFrames will be
 // dispatch to all the downstreams.
-func (s *Server) AddDownstreamServer(addr string, c FrameWriterConnection) {
+func (s *Server) AddDownstreamServer(c Downstream) {
 	s.mu.Lock()
-	s.downstreams[addr] = c
+	s.downstreams[c.ID()] = c
 	s.mu.Unlock()
 }
 
@@ -524,7 +524,11 @@ func (s *Server) dispatchToDownstreams(c *Context) {
 	dataFrame.Metadata = mdBytes
 
 	for _, ds := range s.downstreams {
-		c.Logger.Info("dispatching to downstream", "tid", tid, "sid", sid, "tag", dataFrame.Tag, "data_length", len(dataFrame.Payload), "downstream_id", ds.ClientID())
+		c.Logger.Info(
+			"dispatching to downstream",
+			"tid", tid, "sid", sid, "tag", dataFrame.Tag, "data_length", len(dataFrame.Payload),
+			"downstream_id", ds.ID(), "downstream_name", ds.LocalName(),
+		)
 		_ = ds.WriteFrame(dataFrame)
 	}
 }
