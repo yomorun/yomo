161f2a35bf4d346a57709e098e19b24477ba128d
venjiang <venjiang@gmail.com>
2024-02-02 18:00:00 +0800

    update example 10-ai

--- example/10-ai/app/main.go
+++ example/10-ai/app/main.go
@@ -15,33 +15,20 @@ import (
 )
 
 var (
-	addr = "localhost:9000"
-	tag  = uint32(0x60)
-	api  = "http://localhost:8000/azopenai/chat/completions"
+	addr    = "localhost:9000"
+	tag     = uint32(0x60)
+	sinkTag = uint32(0x61)
+	api     = "http://localhost:8000/azopenai/chat/completions"
 )
 
 func main() {
-	// source
-	source := yomo.NewSource("yomo-source", addr, yomo.WithSourceReConnect())
-	err := source.Connect()
-	if err != nil {
-		slog.Error("[source] ❌ Emit the data to YoMo-Zipper failure with err", "err", err)
-		return
-	}
-	defer source.Close()
-	// set the error handler function when server error occurs
-	source.SetErrorHandler(func(err error) {
-		slog.Error("[source] receive server error", "err", err)
-		os.Exit(1)
-	})
-
 	// sink
-	sink := yomo.NewStreamFunction("sink", "localhost:9000")
-	sink.SetObserveDataTags(0x61)
+	sink := yomo.NewStreamFunction("sink", addr)
+	sink.SetObserveDataTags(sinkTag)
 	sink.SetHandler(func(ctx serverless.Context) {
 		slog.Info("[sink] receive data", "data", string(ctx.Data()))
 	})
-	err = sink.Connect()
+	err := sink.Connect()
 	if err != nil {
 		slog.Error("[sink] connect", "err", err)
 		os.Exit(1)
@@ -49,11 +36,11 @@ func main() {
 	defer sink.Close()
 
 	// app
-	go requestInvokeAIFunction(source)
+	go requestInvokeAIFunction()
 	sink.Wait()
 }
 
-func requestInvokeAIFunction(source yomo.Source) error {
+func requestInvokeAIFunction() error {
 	prompt := "What's the weather like in San Francisco, Melbourne, and Paris?"
 	// invoke ai api
 	req, err := json.Marshal(ai.ChatCompletionsRequest{
@@ -61,7 +48,7 @@ func requestInvokeAIFunction(source yomo.Source) error {
 		Prompt: prompt,
 	})
 	if err != nil {
-		slog.Error("[source] ❌ Marshal data failure with err", "err", err)
+		slog.Error("[app] ❌ Marshal data failure with err", "err", err)
 		return err
 	}
 	// TODO: add bearer token, it's credential
@@ -69,23 +56,23 @@ func requestInvokeAIFunction(source yomo.Source) error {
 	// resp, err := http.Post(api, "application/json", strings.NewReader(req))
 	resp, err := http.Post(api, "application/json", bytes.NewBuffer(req))
 	if err != nil {
-		slog.Error("[source] ❌ Invoke AI function failure with err", "err", err)
+		slog.Error("[app] ❌ Invoke AI function failure with err", "err", err)
 		return err
 	}
 	defer resp.Body.Close()
 	body, err := io.ReadAll(resp.Body)
 	if err != nil {
-		slog.Error("[source] ❌ ReadAll data failure with err", "err", err)
+		slog.Error("[app] ❌ ReadAll data failure with err", "err", err)
 		return err
 	}
 	if resp.StatusCode != http.StatusOK {
-		slog.Warn("[source] ⛔️Invoke AI function failure",
+		slog.Warn("[app] ⛔️Invoke AI function failure",
 			"status", resp.StatusCode,
 			"body", string(body),
 		)
 		return nil
 	}
-	slog.Info("[source] ✅ Invoke AI function", "prompt", prompt)
+	slog.Info("[app] ✅ Invoke AI function", "prompt", prompt)
 
 	return nil
 }
