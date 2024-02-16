dfac256ef40704ec2c52bedd82bca6b90c4a8676
C.C <fanweixiao@gmail.com>
2024-02-16 13:36:03 +0800

    add unit test

--- example/10-ai/sfn-time-zone-calculator/main.go
+++ example/10-ai/sfn-time-zone-calculator/main.go
@@ -1,11 +1,80 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
+	"log/slog"
+	"os"
 	"time"
+
+	"github.com/yomorun/yomo"
+	"github.com/yomorun/yomo/serverless"
 )
 
+type Parameter struct {
+	TimeString     string `json:"timeString" jsonschema:"description=The time string to be converted"`
+	SourceTimezone string `json:"sourceTimezone" jsonschema:"description=The source timezone of the time string, in IANA Time Zone Database identifier format"`
+	TargetTimezone string `json:"targetTimezone" jsonschema:"description=The target timezone to convert the time string to, in IANA Time Zone Database identifier format"`
+}
+
+func Description() string {
+	return "Extract time and timezone information from the following text. The desired format for the time is 'YYYY-MM-DD HH:MM:SS', and the timezone should be specified using an IANA Time Zone Database identifier."
+}
+
+func InputSchema() any {
+	return &Parameter{}
+}
+
 const timeFormat = "2006-01-02 15:04:05"
+
+func main() {
+	sfn := yomo.NewStreamFunction(
+		"fn-timezone-converter",
+		"localhost:9000",
+		yomo.WithSfnCredential("token:Happy New Year"),
+		yomo.WithSfnAIFunctionDefinition(Description(), InputSchema()),
+	)
+	defer sfn.Close()
+
+	sfn.SetObserveDataTags(0x10)
+
+	// start
+	err := sfn.Connect()
+	if err != nil {
+		slog.Error("[sfn] connect", "err", err)
+		os.Exit(1)
+	}
+
+	sfn.SetHandler(handler)
+
+	// set the error handler function when server error occurs
+	sfn.SetErrorHandler(func(err error) {
+		slog.Error("[sfn] receive server error", "err", err)
+	})
+
+	sfn.Wait()
+}
+
+func handler(ctx serverless.Context) {
+	var msg Parameter
+	err := json.Unmarshal(ctx.Data(), &msg)
+	if err != nil {
+		slog.Error("[sfn] json.Marshal error", "err", err)
+		os.Exit(-2)
+	}
+
+	if msg.TargetTimezone == "" {
+		msg.TargetTimezone = "UTC"
+	}
+
+	target, err := ConvertTimezone(msg.TimeString, msg.SourceTimezone, msg.TargetTimezone)
+	if err != nil {
+		slog.Error("[sfn] ConvertTimezone error", "err", err)
+		return
+	}
+
+	ctx.WriteWithTarget(0x61, []byte(target), "user-1")
+}
 
 // ConvertTimezone converts the current time from the source timezone to the target timezone.
 // It returns the converted time as a string in the format "2006-01-02 15:04:05".
