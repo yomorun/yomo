ca6ecf9187f6297483feb41ce7410619aeafb637
C.C <fanweixiao@gmail.com>
2024-02-20 11:20:57 +0800

    improve test for getRates function

--- example/10-ai/sfn-currency-converter/main_test.go
+++ example/10-ai/sfn-currency-converter/main_test.go
@@ -2,17 +2,29 @@
 package main
 
 import (
+	"os/exec"
+	"strconv"
+	"strings"
 	"testing"
+
+	"github.com/stretchr/testify/assert"
 )
 
 func TestGetRates(t *testing.T) {
 	targetCurrency := "AUD"
-	expectedRate := 1.0
-	expectedResult := 1.538269
+
+	cmd := exec.Command("bash", "-c", `cat usd.json | grep "AUD" | awk -F': ' '{print $2}' | sed 's/,//'`)
+	output, err := cmd.Output()
+	assert.NoError(t, err, "can not find jq command")
+
+	outputStr := strings.TrimSpace(string(output)) // remove newline character
+	expectedRate, err := strconv.ParseFloat(outputStr, 64)
+	assert.NoError(t, err, "parse USD.json error")
 
 	actualRate := getRates(targetCurrency)
+	assert.EqualValues(t, actualRate, expectedRate)
 
-	if actualRate != expectedResult {
-		t.Errorf("Expected rate: %f, but got: %f", expectedRate, actualRate)
-	}
+	// if actualRate != expectedResult {
+	// 	t.Errorf("Expected rate: %f, but got: %f", expectedRate, actualRate)
+	// }
 }
