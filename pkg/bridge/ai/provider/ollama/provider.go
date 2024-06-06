f87c71fd8201d63fdc1607127c0614c90472e601
wujunzhuo <wujunzhuo@hotmail.com>
2024-06-06 18:32:22 +0800

    implement stream mode

--- pkg/bridge/ai/provider/ollama/provider.go
+++ pkg/bridge/ai/provider/ollama/provider.go
@@ -35,6 +35,7 @@ type ollamaRequest struct {
 
 type ollamaResponse struct {
 	Response string `json:"response"`
+	Done     bool   `json:"done"`
 }
 
 type templateRequest struct {
@@ -53,8 +54,90 @@ const (
 
 	systemToolExtra = "If the question of the user matched the description of a tool, the tool will be called, and only the function description JSON object should be returned. Don't make assumptions about what values to plug into functions. Ask for clarification if a user request is ambiguous."
 
-	mistralTmpl = "[AVAILABLE_TOOLS] {{.Tools}} [/AVAILABLE_TOOLS][INST] {{ if .System }}{{ .System }} {{ end }}{{ .Prompt }} [/INST]"
+	mistralTmpl = "{{ if .Tools }}[AVAILABLE_TOOLS] {{.Tools}} [/AVAILABLE_TOOLS] {{ end }}[INST] {{ if .System }}{{ .System }} {{ end }}{{ .Prompt }} [/INST]"
 )
+
+func makeOllamaRequestBody(req openai.ChatCompletionRequest) (io.Reader, error) {
+	if !strings.HasPrefix(req.Model, "mistral") {
+		return nil, errors.New("currently only Mistral models are supported, see https://ollama.com/library/mistral")
+	}
+
+	t := &templateRequest{
+		System: defaultSystem,
+		Tools:  "[]",
+	}
+	for _, msg := range req.Messages {
+		switch strings.ToLower(msg.Role) {
+		case openai.ChatMessageRoleSystem:
+			t.System = msg.Content
+		case openai.ChatMessageRoleUser:
+			t.Prompt += msg.Content + " "
+		case openai.ChatMessageRoleTool:
+			t.Prompt += msg.Content + " "
+		}
+	}
+
+	if len(req.Tools) > 0 {
+		t.System += systemToolExtra
+
+		req.Stream = false
+
+		tools, err := json.Marshal(req.Tools)
+		if err != nil {
+			return nil, err
+		}
+		t.Tools = string(tools)
+	}
+
+	ylog.Debug("ollama chat request", "model", req.Model, "system", t.System, "prompt", t.Prompt, "tools", t.Tools)
+
+	tmpl, err := template.New("ollama").Parse(mistralTmpl)
+	if err != nil {
+		return nil, err
+	}
+
+	prompt := bytes.NewBufferString("")
+	err = tmpl.Execute(prompt, t)
+	if err != nil {
+		return nil, err
+	}
+
+	body, err := json.Marshal(&ollamaRequest{
+		Model:  req.Model,
+		Prompt: prompt.String(),
+		Raw:    true,
+		Stream: req.Stream,
+	})
+	if err != nil {
+		return nil, err
+	}
+
+	return bytes.NewBuffer(body), nil
+}
+
+func parseToolCallsFromResponse(response string) []openai.ToolCall {
+	toolCalls := make([]openai.ToolCall, 0)
+
+	response = strings.TrimPrefix(response, "[TOOL_CALLS]")
+	for _, v := range strings.Split(response, "\n") {
+		var functions []mistralFunction
+		if json.Unmarshal([]byte(v), &functions) == nil {
+			for _, f := range functions {
+				arguments, _ := json.Marshal(f.Arguments)
+				toolCalls = append(toolCalls, openai.ToolCall{
+					ID:   id.New(),
+					Type: openai.ToolTypeFunction,
+					Function: openai.FunctionCall{
+						Name:      f.Name,
+						Arguments: string(arguments),
+					},
+				})
+			}
+		}
+	}
+
+	return toolCalls
+}
 
 // GetChatCompletions implements ai.LLMProvider.
 func (p *Provider) GetChatCompletions(ctx context.Context, req openai.ChatCompletionRequest, _ metadata.M) (openai.ChatCompletionResponse, error) {
@@ -66,65 +149,13 @@ func (p *Provider) GetChatCompletions(ctx context.Context, req openai.ChatComple
 			{
 				Index: 0,
 				Message: openai.ChatCompletionMessage{
-					Role:    "assitant",
+					Role:    openai.ChatMessageRoleAssistant,
 					Content: "error occured during inference period",
 				},
-				FinishReason: "stop",
+				FinishReason: openai.FinishReasonStop,
 			},
 		},
 		Usage: openai.Usage{}, // todo
-	}
-
-	if !strings.HasPrefix(req.Model, "mistral") {
-		return res, errors.New("currently only Mistral models are supported, see https://ollama.com/library/mistral")
-	}
-
-	t := &templateRequest{
-		System: defaultSystem,
-		Tools:  "[]",
-	}
-	for _, msg := range req.Messages {
-		switch strings.ToLower(msg.Role) {
-		case "system":
-			t.System = msg.Content
-		case "user":
-			t.Prompt += msg.Content
-		case "tool":
-			t.Prompt += msg.Content
-		}
-	}
-
-	if req.Tools != nil {
-		t.System += systemToolExtra
-
-		tools, err := json.Marshal(req.Tools)
-		if err != nil {
-			return res, err
-		}
-		t.Tools = string(tools)
-	}
-
-	ylog.Debug("ollama chat request", "model", req.Model, "system", t.System, "prompt", t.Prompt, "tools", t.Tools)
-
-	tmpl, err := template.New("ollama").Parse(mistralTmpl)
-	if err != nil {
-		return res, err
-	}
-
-	prompt := bytes.NewBufferString("")
-	err = tmpl.Execute(prompt, t)
-	if err != nil {
-		return res, err
-	}
-
-	body, err := json.Marshal(&ollamaRequest{
-		Model:  req.Model,
-		Prompt: prompt.String(),
-		Raw:    true,
-		Stream: req.Stream,
-	})
-	if err != nil {
-		return res, err
 	}
 
 	urlPath, err := url.JoinPath(p.Endpoint, "api/generate")
@@ -132,51 +163,50 @@ func (p *Provider) GetChatCompletions(ctx context.Context, req openai.ChatComple
 		return res, err
 	}
 
-	resp, err := http.Post(urlPath, "application/json", bytes.NewBuffer(body))
+	body, err := makeOllamaRequestBody(req)
+	if err != nil {
+		return res, err
+	}
+
+	client := http.Client{}
+	request, err := http.NewRequestWithContext(ctx, http.MethodPost, urlPath, body)
+	if err != nil {
+		return res, err
+	}
+
+	request.Header.Set("Content-Type", "application/json")
+
+	resp, err := client.Do(request)
 	if err != nil {
 		return res, err
 	}
 	defer resp.Body.Close()
 
-	body, err = io.ReadAll(resp.Body)
-	if err != nil {
-		return res, err
-	}
-
 	if resp.StatusCode != http.StatusOK {
 		return res, fmt.Errorf("ollama inference error: %s", resp.Status)
 	}
 
-	ylog.Debug("ollama chat response", "body", string(body))
-
-	var o ollamaResponse
-	err = json.Unmarshal(body, &o)
+	buf, err := io.ReadAll(resp.Body)
 	if err != nil {
 		return res, err
 	}
 
-	if o.Response != "" {
-		o.Response = strings.TrimPrefix(o.Response, "[TOOL_CALLS]")
+	var o ollamaResponse
+	err = json.Unmarshal(buf, &o)
+	if err != nil {
+		return res, err
+	}
 
+	ylog.Debug("ollama chat response", "response", o.Response)
+
+	if o.Response != "" {
 		res.Choices[0].Message.Content = o.Response
 
-		var functions []mistralFunction
-		err = json.Unmarshal([]byte(o.Response), &functions)
-		if err == nil {
-			res.Choices[0].FinishReason = "tool_calls"
-			res.Choices[0].Message.ToolCalls = make([]openai.ToolCall, 0)
-			for _, f := range functions {
-				arguments, _ := json.Marshal(f.Arguments)
-				res.Choices[0].Message.ToolCalls = append(
-					res.Choices[0].Message.ToolCalls, openai.ToolCall{
-						ID:   id.New(),
-						Type: openai.ToolTypeFunction,
-						Function: openai.FunctionCall{
-							Name:      f.Name,
-							Arguments: string(arguments),
-						},
-					},
-				)
+		if len(req.Tools) > 0 {
+			toolCalls := parseToolCallsFromResponse(o.Response)
+			if len(toolCalls) > 0 {
+				res.Choices[0].FinishReason = openai.FinishReasonToolCalls
+				res.Choices[0].Message.ToolCalls = toolCalls
 			}
 		}
 	}
@@ -184,9 +214,117 @@ func (p *Provider) GetChatCompletions(ctx context.Context, req openai.ChatComple
 	return res, nil
 }
 
+type streamResponse struct {
+	reader    io.ReadCloser
+	withTools bool
+	index     int
+	res       openai.ChatCompletionStreamResponse
+}
+
+func (s *streamResponse) Recv() (openai.ChatCompletionStreamResponse, error) {
+	var buf []byte
+	var err error
+
+	if s.withTools {
+		buf, err = io.ReadAll(s.reader)
+		if err != nil {
+			return s.res, err
+		}
+	} else {
+		buf = make([]byte, 1024)
+		n, err := s.reader.Read(buf)
+		if err != nil {
+			return s.res, err
+		}
+		buf = buf[:n]
+	}
+
+	ylog.Debug("ollama chat stream", "delta", string(buf))
+	if len(buf) == 0 {
+		s.reader.Close()
+		return s.res, io.EOF
+	}
+
+	var o ollamaResponse
+	err = json.Unmarshal(buf, &o)
+	if err != nil {
+		return s.res, err
+	}
+
+	ylog.Debug("ollama chat stream response", "response", o.Response, "done", o.Done)
+
+	s.res.Choices[0].Index++
+	s.res.Choices[0].Delta.Content = o.Response
+	if o.Done {
+		s.res.Choices[0].FinishReason = openai.FinishReasonStop
+		s.res.Usage = &openai.Usage{} // todo
+
+		if s.withTools {
+			toolCalls := parseToolCallsFromResponse(o.Response)
+			if len(toolCalls) > 0 {
+				for i := 0; i < len(toolCalls); i++ {
+					index := i
+					toolCalls[index].Index = &index
+				}
+				s.res.Choices[0].FinishReason = openai.FinishReasonToolCalls
+				s.res.Choices[0].Delta.ToolCalls = toolCalls
+			}
+		}
+	}
+
+	return s.res, nil
+}
+
 // GetChatCompletionsStream implements ai.LLMProvider.
 func (p *Provider) GetChatCompletionsStream(ctx context.Context, req openai.ChatCompletionRequest, _ metadata.M) (ai.ResponseRecver, error) {
-	panic("unimplemented")
+	urlPath, err := url.JoinPath(p.Endpoint, "api/generate")
+	if err != nil {
+		return nil, err
+	}
+
+	body, err := makeOllamaRequestBody(req)
+	if err != nil {
+		return nil, err
+	}
+
+	client := http.Client{}
+	request, err := http.NewRequestWithContext(ctx, http.MethodPost, urlPath, body)
+	if err != nil {
+		return nil, err
+	}
+
+	request.Header.Set("Content-Type", "application/json")
+	request.Header.Set("Accept", "text/event-stream")
+	request.Header.Set("Cache-Control", "no-cache")
+	request.Header.Set("Connection", "keep-alive")
+
+	resp, err := client.Do(request)
+	if err != nil {
+		return nil, err
+	}
+
+	if resp.StatusCode != http.StatusOK {
+		return nil, fmt.Errorf("ollama inference error: %s", resp.Status)
+	}
+
+	return &streamResponse{
+		reader:    resp.Body,
+		withTools: len(req.Tools) > 0,
+		index:     0,
+		res: openai.ChatCompletionStreamResponse{
+			ID:      "chatcmpl-" + id.New(29),
+			Model:   req.Model,
+			Created: time.Now().Unix(),
+			Choices: []openai.ChatCompletionStreamChoice{
+				{
+					Index: -1,
+					Delta: openai.ChatCompletionStreamChoiceDelta{
+						Role: openai.ChatMessageRoleAssistant,
+					},
+				},
+			},
+		},
+	}, nil
 }
 
 // Name implements ai.LLMProvider.
