d8d60346b522878fd6978c3d3dbd562c40f0df6d
woorui <rui1009479218@gmail.com>
2024-12-12 01:55:22 +0800

    stable test

--- main_test.go
+++ main_test.go
@@ -5,14 +5,10 @@ import (
 	"context"
 	"os"
 	"testing"
-	"time"
 )
 
 func TestMain(m *testing.M) {
-	// setup zipper
 	go RunZipper(context.TODO(), "test/config.yaml")
-	time.Sleep(time.Second)
-
 	code := m.Run()
 	os.Exit(code)
 }
