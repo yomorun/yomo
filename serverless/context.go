1f653200953ec6339440940c3c611da7d48ab273
venjiang <venjiang@gmail.com>
2025-06-05 12:05:15 +0800

    remove serverless http interface

--- serverless/context.go
+++ serverless/context.go
@@ -13,8 +13,6 @@ type Context interface {
 	Metadata(string) (string, bool)
 	// Write writes data
 	Write(tag uint32, data []byte) error
-	// HTTP http interface
-	HTTP() HTTP
 	// WriteWithTarget writes data to sfn instance with specified target
 	WriteWithTarget(tag uint32, data []byte, target string) error
 	// ReadLLMArguments reads LLM function arguments
@@ -29,32 +27,6 @@ type Context interface {
 type CronContext interface {
 	// Write writes data
 	Write(tag uint32, data []byte) error
-	// HTTP http interface
-	HTTP() HTTP
 	// WriteWithTarget writes data to sfn instance with specified target
 	WriteWithTarget(tag uint32, data []byte, target string) error
-}
-
-// HTTP http interface
-type HTTP interface {
-	Send(req *HTTPRequest) (*HTTPResponse, error)
-	Get(url string) (*HTTPResponse, error)
-	Post(url string, contentType string, body []byte) (*HTTPResponse, error)
-}
-
-// HTTPRequest http request
-type HTTPRequest struct {
-	Method  string            // GET, POST, PUT, DELETE, ...
-	URL     string            // https://example.org
-	Header  map[string]string // {"Content-Type": "application/json"}
-	Timeout int64             // timeout in milliseconds
-	Body    []byte            // request body
-}
-
-// HTTPResponse http response
-type HTTPResponse struct {
-	Status     string            // "200 OK"
-	StatusCode int               // 200, 404, ...
-	Header     map[string]string // {"Content-Type": "application/json"}
-	Body       []byte            // response body
 }
