32cf749ef60ec1004ef5fbf4a9421e67437d1517
woorui <rui1009479218@gmail.com>
2025-06-20 16:40:04 +0800

    fix: gemini provider

--- pkg/bridge/ai/provider/gemini/provider.go
+++ pkg/bridge/ai/provider/gemini/provider.go
@@ -3,13 +3,10 @@ package gemini
 
 import (
 	"context"
-	"log"
 
 	openai "github.com/sashabaranov/go-openai"
 	"github.com/yomorun/yomo/core/metadata"
 	"github.com/yomorun/yomo/pkg/bridge/ai/provider"
-	"google.golang.org/api/option"
-	"google.golang.org/api/transport"
 )
 
 // Provider is the provider for google gemini.
@@ -22,19 +19,14 @@ var _ provider.LLMProvider = &Provider{}
 
 // NewProvider creates a new gemini provider.
 func NewProvider(apiKey string) *Provider {
-	httpClient, _, err := transport.NewHTTPClient(context.Background(), option.WithAPIKey(apiKey))
-	if err != nil {
-		log.Fatalln("gemini new http client: ", err)
-	}
+	c := openai.DefaultConfig(apiKey)
+	c.BaseURL = "https://generativelanguage.googleapis.com/v1beta/openai/"
+	c.EmptyMessagesLimit = 300
 
-	client := openai.NewClientWithConfig(openai.ClientConfig{
-		BaseURL:            "https://generativelanguage.googleapis.com/v1beta/openai/",
-		HTTPClient:         httpClient,
-		EmptyMessagesLimit: 300,
-	})
+	client := openai.NewClientWithConfig(c)
 
 	return &Provider{
-		model:  "gemini-1.5-pro-latest",
+		model:  "gemini-2.0-flash",
 		client: client,
 	}
 }
@@ -43,8 +35,6 @@ func NewProvider(apiKey string) *Provider {
 func (p *Provider) GetChatCompletions(ctx context.Context, req openai.ChatCompletionRequest, md metadata.M) (openai.ChatCompletionResponse, error) {
 	if req.Model == "" {
 		req.Model = p.model
-	} else {
-		req.Model = "google/" + req.Model
 	}
 	return p.client.CreateChatCompletion(ctx, req)
 }
@@ -53,8 +43,6 @@ func (p *Provider) GetChatCompletions(ctx context.Context, req openai.ChatComple
 func (p *Provider) GetChatCompletionsStream(ctx context.Context, req openai.ChatCompletionRequest, md metadata.M) (provider.ResponseRecver, error) {
 	if req.Model == "" {
 		req.Model = p.model
-	} else {
-		req.Model = "google/" + req.Model
 	}
 
 	return p.client.CreateChatCompletionStream(ctx, req)
