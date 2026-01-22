89542d288c92c201ae2bf50648b145c1256ef4a9
woorui <rui1009479218@gmail.com>
2026-01-22 22:44:58 +0800

    feat: cache tool call extra

--- pkg/bridge/ai/provider/vertexai/provider.go
+++ pkg/bridge/ai/provider/vertexai/provider.go
@@ -4,8 +4,11 @@ package vertexai
 import (
 	"context"
 	"fmt"
+	"io"
 	"log"
+	"time"
 
+	"github.com/hashicorp/golang-lru/v2/expirable"
 	openai "github.com/sashabaranov/go-openai"
 	"github.com/yomorun/yomo/core/metadata"
 	"github.com/yomorun/yomo/pkg/bridge/ai/provider"
@@ -15,8 +18,9 @@ import (
 
 // Provider is the provider for google vertexai.
 type Provider struct {
-	model  string
-	client *openai.Client
+	model       string
+	client      *openai.Client
+	thoughtSign *thoughtSignature
 }
 
 var _ provider.LLMProvider = &Provider{}
@@ -52,8 +56,9 @@ func NewProvider(projectID, location, model, credentialsFile string) *Provider {
 	model = "google/" + model
 
 	return &Provider{
-		model:  model,
-		client: client,
+		model:       model,
+		client:      client,
+		thoughtSign: newThoughtSignature(),
 	}
 }
 
@@ -65,7 +70,18 @@ func (p *Provider) GetChatCompletions(ctx context.Context, req openai.ChatComple
 		req.Model = "google/" + req.Model
 	}
 
-	return p.client.CreateChatCompletion(ctx, req)
+	p.thoughtSign.AttachExtra(req)
+
+	resp, err := p.client.CreateChatCompletion(ctx, req)
+	if err != nil {
+		return openai.ChatCompletionResponse{}, err
+	}
+
+	for _, choice := range resp.Choices {
+		p.thoughtSign.SaveExtra(choice.Message.ToolCalls)
+	}
+
+	return resp, nil
 }
 
 // GetChatCompletionsStream implements provider.LLMProvider.
@@ -75,11 +91,66 @@ func (p *Provider) GetChatCompletionsStream(ctx context.Context, req openai.Chat
 	} else {
 		req.Model = "google/" + req.Model
 	}
+	p.thoughtSign.AttachExtra(req)
 
-	return p.client.CreateChatCompletionStream(ctx, req)
+	stream, err := p.client.CreateChatCompletionStream(ctx, req)
+	if err != nil {
+		return nil, err
+	}
+	return &recver{resp: stream}, nil
 }
 
 // Name implements provider.LLMProvider.
 func (p *Provider) Name() string {
 	return "vertexai"
+}
+
+type recver struct {
+	resp        *openai.ChatCompletionStream
+	thoughtSign *thoughtSignature
+	toolCalls   []openai.ToolCall
+}
+
+func (r *recver) Recv() (openai.ChatCompletionStreamResponse, error) {
+	resp, err := r.resp.Recv()
+	if err == io.EOF {
+		r.thoughtSign.SaveExtra(r.toolCalls)
+	}
+	if err != nil {
+		return openai.ChatCompletionStreamResponse{}, err
+	}
+	for _, choice := range resp.Choices {
+		r.toolCalls = append(r.toolCalls, choice.Delta.ToolCalls...)
+	}
+	return resp, nil
+}
+
+type thoughtSignature struct {
+	inner *expirable.LRU[string, map[string]any]
+}
+
+func newThoughtSignature() *thoughtSignature {
+	return &thoughtSignature{
+		inner: expirable.NewLRU[string, map[string]any](200, nil, time.Hour),
+	}
+}
+
+func (k *thoughtSignature) SaveExtra(toolCalls []openai.ToolCall) {
+	for _, toolCall := range toolCalls {
+		if extra := toolCall.ExtraContent; extra != nil && extra["google"] != nil {
+			k.inner.Add(toolCall.ID, extra)
+		}
+	}
+}
+
+func (k *thoughtSignature) AttachExtra(req openai.ChatCompletionRequest) {
+	for i, msg := range req.Messages {
+		for j, toolCall := range msg.ToolCalls {
+
+			if extra, ok := k.inner.Get(toolCall.ID); ok {
+				// k.inner.Remove(toolCall.ID)
+				req.Messages[i].ToolCalls[j].ExtraContent = extra
+			}
+		}
+	}
 }
